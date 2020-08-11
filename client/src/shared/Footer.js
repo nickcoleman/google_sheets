5c23b6231f42b888760ae09771e20f8e3abace3f
nickcoleman <nick@nickcoleman.io>
2020-08-11 16:13:16 -0600

    responsive footer image

--- client/src/shared/Footer.js
+++ client/src/shared/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Container from '@material-ui/core/Container';
@@ -11,11 +12,9 @@ const Footer = ({logo}) => {
   const classes = useStyles();
   return (
     <Container align='center' className={classes.container}>
-      <Card className={classes.card} elevation={0}>
-        <CardMedia title='Refined Harmony' alt='Refined Harmony'>
-          <img src={logo} className={classes.media} />
-        </CardMedia>
-      </Card>
+      <Box className={classes.imageBox}>
+        <img src={logo} className={classes.media} />
+      </Box>
       <Typography className={classes.content}>
         DOING BUSINESS AS PREMIER MARTIAL ARTS UTAH
       </Typography>
--- client/src/shared/Footer.styles.js
+++ client/src/shared/Footer.styles.js
@@ -2,16 +2,16 @@ import {makeStyles} from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    flexShrink: 0,
+    // flexShrink: 0,
     marginTop: 50,
   },
-  card: {
+  imageBox: {
     margin: '20px auto',
     maxWidth: 350,
   },
   media: {
-    width: 'auto',
-    height: 60,
+    width: '90%',
+    height: 'auto',
   },
   content: {},
 }));
