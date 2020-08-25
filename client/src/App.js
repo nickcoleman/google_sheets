4d9d9dcc20e351e6e30c5c4594a7492d58e8450a
David Fischer <davidfisc@hotmail.com>
2020-08-25 17:04:33 -0600

    added new routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,12 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {ThemeProvider} from '@material-ui/core';
 import theme from './theme';
 
-import Promotion from './landing_pages/Promotion/Promotion';
+import Form from './landing_pages/Promotion/Promotion';
 import ThankYou from './landing_pages/ThankYou/ThankYou';
-import LearnMore from './landing_pages/LearnMore/LearnMore';
+import PMA from './landing_pages/LearnMore/LearnMore';
+
+import privacyPDF from './assets/react.pdf'
+import termsPDF from './assets/react.pdf'
 
 import './App.css';
 
@@ -15,10 +18,19 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Switch>
-          <Route path='/learnmore' component={LearnMore} />
-          <Route path='/promotion' component={Promotion} />
-          <Route path='/thankyou' component={ThankYou} />
-          <Route path='/' component={Promotion} />
+          <Route
+            path="/privacy"
+            render={() => <iframe title="privacy policy" style={{height: '100vh', width: '100vw'}} src={privacyPDF}></iframe>}
+          />
+          <Route
+            path="/terms"
+            render={() => <iframe title="terms of service" style={{height: '100vh', width: '100vw'}} src={termsPDF}></iframe>}
+          />
+          <Route path="/terms" component={Form} />
+          <Route path="/form" component={Form} />
+          <Route path="/home" component={PMA} />
+          <Route path="/thankyou" component={ThankYou} />
+          <Route path="/" component={PMA} />
         </Switch>
       </Router>
     </ThemeProvider>
