75fad463ceb480e9ee0858a589e49b53ebdf1fba
David Fischer <davidfisc@hotmail.com>
2020-08-26 15:47:55 -0600

    updated to use new server, created new keys

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://ma-server-285520.wl.r.appspot.com';
+export const BASE_URL = "https://server-287604.wn.r.appspot.com";
 // export const BASE_URL = 'localhost:4000';
 
 export const BASE_URL_LANDING = `${BASE_URL}/landing`;
